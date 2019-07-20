594d9ba5aae479e20877539f4bfb691e4294250e
Zz <393103982@qq.com>
2019-07-20 16:19:54 +0800

    Add 1.0.0 新建项目
    预知功能如下
    
    KJPlayer是一款视频播放器，AVPlayer的封装，继承UIView
    
    - 支持播放网络和本地视频
    - 支持播放多种格式mp4、m3u8、3gp、mov等等
    - 视频可以边下边播，把播放器播放过的数据流缓存到本地，下次播放直接从缓冲读取播放
    - 支持拖动、手势快进倒退、增大减小音量等等

---
+++ KJPlayerDemo/Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KJPlayerDemo
+//
+//  Created by 杨科军 on 2019/7/20.
+//  Copyright © 2019 杨科军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ KJPlayerDemo/Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KJPlayerDemo
+//
+//  Created by 杨科军 on 2019/7/20.
+//  Copyright © 2019 杨科军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
